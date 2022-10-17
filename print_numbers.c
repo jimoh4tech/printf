27e833ca23f80f0c4c31dc6e65f8d1be77ddb8d2
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-10-17 10:07:24 -0700

    Fix betty for function

--- print_numbers.c
+++ print_numbers.c
@@ -41,82 +41,46 @@ return (i);
 
 
 /**
-
  * print_octal - prints octal number
-
  * @n: number to be printed
-
  *
-
  * Return: length of the number printed
-
  */
 
-
-
-int print_octal(unsigned int n)
-  
+int print_octal(unsigned int n) 
 {
-  
-  char *s;
-  
-  s = decimal_to_any(n, 8);
-  
-  return(_puts(s));
-  
+char *s;
+s = decimal_to_any(n, 8);  
+return(_puts(s));  
 }
 
 /**
-
  * print_binary - prints binary number
-
  * @n: number to be printed
-
  *
-
  * Return: length of the number printed
-
  */
-
-
 
 int print_binary(unsigned int n)
-  
 {
-  
-  char *s;
-  
-  s = decimal_to_any(n, 2);
-  
-  return(_puts(s));
-  
+char *s;
+s = decimal_to_any(n, 2);
+return(_puts(s));  
 }
 
 
 /**
-
  * print_hexadecimal - prints hex number uppercase
-
  * @n: number to be printed
-
  *
-
  * Return: length of the number printed
-
  */
 
-
-
 int print_hexadecimal(unsigned int n)
-  
 {
-  
-  char *s;
-  
-  s = decimal_to_any(n, 16);
-  
-  return(_puts(s));
-  
+char *s;
+s = decimal_to_any(n, 16);  
+return(_puts(s));  
 }
 
 /**
